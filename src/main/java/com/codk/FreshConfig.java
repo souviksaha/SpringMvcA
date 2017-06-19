2eecbe327217bb15a3aa3dd141bb66cfe25e0d84
souviksaha <souviksaha1215@gmail.com>
2017-06-18 19:50:19 -0700

    first commit

---
+++ src/main/java/com/codk/FreshConfig.java
@@ -0,0 +1,10 @@
+package com.codk;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.codk"})
+public class FreshConfig {
+
+}
---
+++ src/main/java/com/codk/service/AddService.java
@@ -0,0 +1,10 @@
+package com.codk.service;
+
+public class AddService {
+
+	public int add(int i,int j)
+	{
+		return i+j;
+	}
+	
+}//end of AddService
